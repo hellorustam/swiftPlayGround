bb8ce7229d88fba944907245470a48b588626e48
Rustam S <blakfoh@gmail.com>
2022-03-27 10:43:51 +0300

    предварительные записи

--- Animal.playground/Contents.swift
+++ Animal.playground/Contents.swift
@@ -7,20 +7,38 @@ enum Color: String {
 }
 
 class Animal {
-    var weight: Int
+    var weightMain: Int
     var voice: String
     var color: String
     
     init(weight: Int, voice: String, color: String){
-        self.weight = weight
         self.voice = voice
         self.color = color
+        self.weightMain = weight
+    }
+    
+//    init(weight: Int){
+//        self.weightMain = weight
+//    }
+    
+    func printWeight(text: String) {
+       print(weightMain)
+//       print(color)
     }
 }
 
 class Cat: Animal{
+//    override init(weight: Int, voice: String, color: String) {
+//        super.init(weight: weight, voice: voice, color: color){
+//            self.weight = weightMain + 1
+//        }
+//    }
+    
+//    weightMain += 1
+    
     func voiceCat(voice: String) {
         print("Cat say \(voice)")
+        print(weightMain)
     }
 }
 
@@ -28,8 +46,8 @@ class Dog: Animal {
     
 }
 
-let cat1 = Cat(weight: 45, voice: "myau-myau", color: Color.black.rawValue)
+let cat1 = Cat(weight: 45, voice: "myau-myau", color: Color.yellow.rawValue)
+//let cat2 = Cat(weight: 45)
 
-cat1.voiceCat(voice: "asd")
 
-print(cat1.color)
+cat1.voiceCat(voice: "String")
